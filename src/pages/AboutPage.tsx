eb97f1045d24a39fe39cb33c07d8b9bac36bb642
Seu Nome <seu-email@exemplo.com>
2025-08-01 23:37:49 -0300

    .

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -64,7 +64,7 @@ const AboutPage: React.FC = () => {
               <div className="absolute -bottom-6 -left-6 w-40 h-40 bg-primary-100 rounded-lg z-0"></div>
               <div className="absolute -top-6 -right-6 w-40 h-40 bg-secondary-100 rounded-lg z-0"></div>
               <img
-                src="/equipe.webp"
+                src="/equipe_copiar.webp"
                 alt="Escritório AMS Advogados"
                 className="relative z-10 w-full h-auto rounded-lg shadow-xl"
               />
